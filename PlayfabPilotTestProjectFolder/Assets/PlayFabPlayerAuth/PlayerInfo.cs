0ba561dec8353a965a84556e46d41971df0bc1cd
pkcody <77869251+pkcody@users.noreply.github.com>
2022-02-04 13:06:32 -0700

    assets

---
+++ PlayfabPilotTestProjectFolder/Assets/PlayFabPlayerAuth/PlayerInfo.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayerInfo : MonoBehaviour
+{
+    [HideInInspector]
+    public PlayerProfileModel profile;
+
+    public static PlayerInfo instance;
+    void Awake() { instance = this; }
+
+    public void OnLoggedIn()
+    {
+        GetPlayerProfileRequest getProfileRequest = new GetPlayerProfileRequest
+        {
+            PlayFabId = LoginRegister.instance.playFabId,
+            ProfileConstraints = new PlayerProfileViewConstraints
+            {
+                ShowDisplayName = true
+            }
+        };
+
+        PlayFabClientAPI.GetPlayerProfile(getProfileRequest,
+            result =>
+            {
+            profile = result.PlayerProfile;
+                LoginRegister.instance.SetDisplayText("Loaded in player: " + profile.DisplayName, Color.green);
+            },
+            error => LoginRegister.instance.SetDisplayText(error.ErrorMessage, Color.red)
+            );
+
+    }
+}
